c9b24f2f8c52a5f350be7900cc61bacf50f5838c
XikoCat <guigasreis99@gmail.com>
2021-12-27 03:13:41 +0000

    Day 6

---
+++ Day6/chal_1.py
@@ -0,0 +1,33 @@
+input = "Day6/input.txt"
+
+
+def simulateDay(pool: list):
+    child_count = 0
+    for index, fish in enumerate(pool):
+        if fish == 0:
+            child_count += 1
+            pool[index] = 6
+        else:
+            pool[index] -= 1
+    
+    while child_count > 0:
+        pool.append(8)
+        child_count -= 1
+
+    return pool
+        
+
+def main():
+    with open(input) as f:
+        lanternfish = [int(i) for i in f.readline().strip().split(',')]
+    
+    #print(lanternfish)
+    
+    for day in range(80):
+        lanternfish = simulateDay(lanternfish)
+        #print(day, len(lanternfish))
+
+    print(len(lanternfish))
+
+if __name__ == "__main__":
+    main()
